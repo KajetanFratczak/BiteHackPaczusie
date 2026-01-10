ccb0a351e1a9f184513637c3f700a7f4449ba2bc
Simi1354 <sbalicki@student.agh.edu.pl>
2026-01-10 15:40:20 +0100

    login page in progress

--- frontend/paczusie_frontend/src/App.js
+++ frontend/paczusie_frontend/src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css';
 import HomePage from './pages/HomePage';
 import AdPage from './pages/AdPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />  
-        <Route path="/ad" element={<AdPage />} />    
+        <Route path="/:id" element={<AdPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
