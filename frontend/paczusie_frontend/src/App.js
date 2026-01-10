1540af7109539aad6448a9b684ed14558814e577
Kajetan Frątczak <kfratczak@student.agh.edu.pl>
2026-01-10 15:38:40 +0100

    AuthContext done (register tba)

--- frontend/paczusie_frontend/src/App.js
+++ frontend/paczusie_frontend/src/App.js
@@ -2,14 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.css';
 import HomePage from './pages/HomePage';
 import AdPage from './pages/AdPage';
+import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />  
-        <Route path="/ad" element={<AdPage />} />    
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />  
+          <Route path="/ad" element={<AdPage />} />    
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
